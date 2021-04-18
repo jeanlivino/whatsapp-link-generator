cfd569dc5ebf1410ff3c517fdaec9c6df490930a
Jean Livino <jeanlivino@gmail.com>
2021-04-18 17:11:53 -0300

    refactor: 💡 disable save to db

--- pages/go.js
+++ pages/go.js
@@ -1,41 +1,31 @@
-import { connectToDatabase } from "../utils/mongodb";
 import React from 'react'
 import uaParser from 'ua-parser-js'
+
 import { isDev } from '../utils/is'
 
-const goPage = (props) => {
-	return (
-		<div>
-			{JSON.stringify(props)}
-		</div>
-	)
-}
+const goPage = props => <div>{JSON.stringify(props)}</div>
 
 export async function getServerSideProps({ req, query }) {
-	const ua = uaParser(req.headers['user-agent'])
-	const { p, m } = query;
+  const ua = uaParser(req.headers['user-agent'])
+  const { p, m } = query
 
-	const { type = 'desktop' } = ua.device
-	const url = type === 'desktop' ? `https://web.whatsapp.com/send?phone=${p}&text=${m}` : `whatsapp://send?phone=${p}&text=${m}`
-	const content = { phone: p, message: m, type }
-	if (isDev()) return {
-		props: content
-	}
+  const { type = 'desktop' } = ua.device
+  const url =
+    type === 'desktop'
+      ? `https://web.whatsapp.com/send?phone=${p}&text=${m}`
+      : `whatsapp://send?phone=${p}&text=${m}`
+  const content = { phone: p, message: m, type }
+  if (isDev())
+    return {
+      props: content,
+    }
 
-
-	try {
-		{
-			const { db } = await connectToDatabase();
-			await db.collection("clicks").insertOne(content)
-		}
-	} finally {
-		return {
-			props: {},
-			redirect: {
-				destination: url,
-				permanent: false,
-			},
-		}
-	}
+  return {
+    props: {},
+    redirect: {
+      destination: url,
+      permanent: false,
+    },
+  }
 }
 export default goPage
