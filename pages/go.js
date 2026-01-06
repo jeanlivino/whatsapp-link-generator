89c5b0ab9170706fb0a3c6dd2d6357c3ae84dd22
Jean Livino <jeanlivino@gmail.com>
2026-01-06 14:49:31 -0300

    fix(go): improve phone number handling and URL construction

--- pages/go.js
+++ pages/go.js
@@ -9,7 +9,7 @@ const goPage = ({ url }) => {
   return ''
 }
 
-const addCountryCode = (phone) => {
+const addCountryCode = phone => {
   if (phone.length === 11 || phone.length === 10) {
     return `55${phone}`
   }
@@ -22,11 +22,19 @@ export async function getServerSideProps({ req, query }) {
 
   const phone = addCountryCode(p)
 
+  const searchParams = new URLSearchParams()
+  if (p && p !== '55') {
+    searchParams.append('phone', phone)
+  }
+  searchParams.append('text', m)
+
+  const queryString = searchParams.toString()
+
   const { type = 'desktop' } = ua.device
   const url =
     type === 'desktop'
-      ? `https://web.whatsapp.com/send?phone=${phone}&text=${m}`
-      : `whatsapp://send?phone=${phone}&text=${m}`
+      ? `https://web.whatsapp.com/send?${queryString}`
+      : `whatsapp://send?${queryString}`
 
   return {
     props: {
